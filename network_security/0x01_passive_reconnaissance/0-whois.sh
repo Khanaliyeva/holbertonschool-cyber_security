f73aa081ef3a26d9081f49ee39e23c067d9ee73f
Khanaliyeva <gunelxaneliyeva2004@gmail.com>
2025-08-17 19:29:16 +0000

    1

--- network_security/0x01_passive_reconnaissance/0-whois.sh
+++ network_security/0x01_passive_reconnaissance/0-whois.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-whois $1 | awk -F ': ' '/^Registrant/ || /^Admin/ || /^Tech/ {print $1 "," $2}' > "$1.csv" 
+whois "$1" | awk -F': ' '/^(Registrant|Admin|Tech)/{section=$1;next} section && $2{gsub(/^[[:space:]]+|[[:space:]]+$/,"",$2);print section " " $1","$2}' > "$1.csv"
